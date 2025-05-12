f01095c035abbd61afcdcc97f9ed92fdc10d356f
flori4n_ <91465120+florianHamal@users.noreply.github.com>
2025-05-12 20:07:09 +0200

    Update lspconfig.lua
    added support for lombock (java)

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -1,6 +1,11 @@
 local M = {}
 local map = vim.keymap.set
 
+local home = os.getenv 'HOME'
+local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = workspace_path .. project_name
+
 -- export on_attach & capabilities
 M.on_attach = function(_, bufnr)
   local function opts(desc)
@@ -67,7 +72,27 @@ M.defaults = function()
       },
     },
   })
-  vim.lsp.config('jdtls', { cmd = { 'jdtls' } })
+  vim.lsp.config('jdtls', { cmd = {
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+    '-jar',
+    vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+    '-configuration',
+    home .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
+    '-data',
+    workspace_dir,
+  } })
   vim.lsp.enable('jdtls')
   vim.lsp.enable "lua_ls"
   --needs typesctipt_language_server npm package
